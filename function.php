8e4da57d4c9287f46702a65c597a3796cc6c9782
ZidannAR <zidanalrajib@gmail.com>
2024-10-26 22:53:14 +0700

    aduh lee

--- function.php
+++ function.php
@@ -30,6 +30,22 @@ function tambah_menu($data)
 
 function upload()
 {
-       
+       $namafile = $_FILES['gambar']['name'];
+       $ukuranfile = $_FILES['gambar']['size'];
+       $error = $_FILES['gambar']['error'];
+       $tmpName = $_FILES['gambar']['tmp_name'];
+
+       if($error === 4){
+              echo "<script> alert('pilih gambar terlebih dahulu')</script>";
+              return false;
+       }
+
+       $ekstensiGambarValid = ['jpg','jpeg','png','gif'];
+       $ekstensiGambar = explode('.',$namafile);
+       $ekstensiGambar = strtolower( end($ekstensiGambar));
+
+       if (!in_array($ekstensiGambar,$ekstensiGambarValid)){
+              
+       }
 }
  ?>
